93d639487c671a85b813527b112bee52ac772af9
Drew Wilson <drew@geedew.com>
2013-09-17 07:36:39 -0500

    Adding test page

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,33 +1,32 @@
 
 // Catch messagse from the Injected JS
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+function(request, sender, sendResponse) {
+  chrome.pageAction.show(sender.tab.id);
 
-    // draw a number on the icon
-  	setPageActionIcon(sender.tab.id);
-
-    sendResponse();
+  // draw a number on the icon
+  setPageActionIcon(sender.tab.id);
+  sendResponse();
 });
 
 // Attempting to create an icon that can show numbers in the icon.
 function setPageActionIcon(tabId) {
-    var canvas = document.createElement('canvas');
-    var img = document.createElement('img');
-    img.onload = function () {
-        var context = canvas.getContext('2d');
-        context.drawImage(img, 0, 2);
-        context.fillStyle = "rgba(255,0,0,1)";
-        context.fillRect(10, 0, 19, 19);
-        context.fillStyle = "white";
-        context.font = "11px Arial";
-        context.fillText("3", 0, 19);
+  var canvas = document.createElement('canvas');
+  var img = document.createElement('img');
+  img.onload = function () {
+    var context = canvas.getContext('2d');
+    context.drawImage(img, 0, 2);
+    context.fillStyle = "rgba(255,0,0,1)";
+    context.fillRect(10, 0, 19, 19);
+    context.fillStyle = "white";
+    context.font = "11px Arial";
+    context.fillText("333", 0, 19);
 
-        chrome.pageAction.setIcon({
-            imageData: context.getImageData(0, 0, 19, 19),
-            tabId:     tabId
-        });
-    };
+    chrome.pageAction.setIcon({
+      imageData: context.getImageData(0, 0, 19, 19),
+      tabId: tabId
+    });
+  };
 }
 
 
